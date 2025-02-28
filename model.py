f4a61d0c688b72c88399cd8ef4a80974814a3b5d
Alana-C <ac2249@byu.edu>
2025-02-28 10:25:14 -0700

    B3: Training the Model

--- model.py
+++ model.py
@@ -3,4 +3,10 @@ from sklearn.model_selection import train_test_split
 import pandas as pd
 import numpy as np
 
+df = pd.read_csv('data/mushrooms.csv')
+X = df.drop('class', axis=1)
+y = df['class']
+x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
 clf = RandomForestClassifier(n_estimators=100)
+clf.fit(X, y)
